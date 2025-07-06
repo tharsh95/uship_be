c2db0ba097f2191c482f287b6421641e3afa54f2
Harsh Tiwari <harshtiwari@Harshs-MacBook-Air.local>
2025-07-06 06:49:56 +0530

    installed cors type

--- src/index.ts
+++ src/index.ts
@@ -17,20 +17,6 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-// Additional CORS headers as backup
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  
-  if (req.method === 'OPTIONS') {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
-
 // JWT middleware
 app.use((req, res, next) => {
   const auth = req.headers['authorization'];
